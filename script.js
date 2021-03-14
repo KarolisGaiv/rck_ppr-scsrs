22194f595009b5d3f6c2e8ce7ce28e23ec5527d2
KarolisGaiv <kgaivenis@gmail.com>
2021-03-14 20:00:03 +0200

    Add computer play function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+console.log("Welcome to Rock Paper Scissors game!");
+
+function computerPlay() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  let number = Math.floor(Math.random() * 3);
+  let computerChoice = choices[number];
+  return computerChoice;
+}
+
+console.log(computerPlay());
