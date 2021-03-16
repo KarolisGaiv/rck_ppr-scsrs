af4c58138b8cc9e1fd3288384ae88590aee9f32a
KarolisGaiv <kgaivenis@gmail.com>
2021-03-16 22:17:45 +0200

    Add round result

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const choices = ["Rock", "Paper", "Scissors"];
 const compHead = document.querySelector(".computer-choice");
 const userHead = document.querySelector(".user-choice");
 const explanation = document.querySelector(".explanation");
+const result = document.querySelector(".result");
 const buttons = document.querySelectorAll("button");
 let user;
 let computer;
@@ -21,27 +22,32 @@ function playerChoice(e) {
   user = e.target.innerText;
   userHead.textContent = user;
   computerPlay();
-  winner();
+  playRound();
   return user;
 }
 
-function winner() {
-  console.log(user + " User");
-  console.log(computer + " Computer");
+function playRound() {
   if (user === "Rock" && computer === "Paper") {
     explanation.textContent = "Paper covers Rock, You lost!";
+    result.textContent = "Lost";
   } else if (user === "Rock" && computer === "Scissors") {
     explanation.textContent ="Rock crushes Scissors, You Won!";
+    result.textContent = "Won";
   } else if (user === "Paper" && computer === "Scissors") {
     explanation.textContent ="Scissors cut Paper, You Lost!";
+    result.textContent = "Lost";
   } else if (user === "Scissors" && computer === "Rock") {
     explanation.textContent ="Rock crushes Scissors, You Lost!";
+    result.textContent = "Lost";
   } else if (user === "Scissors" && computer === "Paper") {
     explanation.textContent ="Scissors cut Paper, You Won!";
+    result.textContent = "Won";
   } else if (user === "Paper" && computer === "Rock") {
     explanation.textContent ="Paper covers Rock, You Won!";
+    result.textContent = "Won";
   } else {
     explanation.textContent =("It's a TIE!");
+    result.textContent = "Tie";
   }
 }
 
